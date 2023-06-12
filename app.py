37b1de240842146c39a3bfaf724465c79e905d37
Boaz <boazde@gmail.com>
2023-06-12 17:18:38 -0400

    add open ai key to streamlit

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ def main_app():
     end_date = st.date_input("End Date", value=datetime.today())
     chat_type = st.selectbox("Chat Type", ["WhatsApp", "Signal"])
     newsletter = st.checkbox("Generate Newsletter Introduction")
+    openai_key = st.text_input("OpenAI API Key")
 
     # Generate Summary
     if st.button("Generate Summary"):
-        if not chat_export_file or not summary_file:
+        if not chat_export_file or not summary_file or not openai_key:
             st.error("Please fill in all the required fields.")
         elif start_date > end_date:
             st.error("Start Date should be earlier than End Date.")
@@ -27,7 +28,7 @@ def main_app():
             st.info("Generating Summary...")
             start_date_str = start_date.strftime("%m/%d/%Y")
             end_date_str = end_date.strftime("%m/%d/%Y")
-            main(chat_type, chat_export_file.name, summary_file, start_date_str, end_date_str, newsletter)
+            main(chat_type, chat_export_file.name, summary_file, start_date_str, end_date_str, newsletter, openai_key)
             st.success("Summary generated successfully!")
 
 # Run the app
