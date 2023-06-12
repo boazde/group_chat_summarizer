245495641e1125a2baccabe24bd12a0fc4f9bc32
Boaz <boazde@gmail.com>
2023-06-12 17:13:34 -0400

    add data and file import

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import streamlit as st
 from group_chat_summarizer import main
+from datetime import datetime
 
 def main_app():
     st.title("Group Chat Summarizer")
 
     # User Input
-    chat_export_file = st.text_input("Chat Export File")
+    chat_export_file = st.file_uploader("Chat Export File")
     summary_file = st.text_input("Summary Output File")
-    start_date = st.text_input("Start Date (MM/DD/YYYY)")
-    end_date = st.text_input("End Date (MM/DD/YYYY)")
+    start_date = st.date_input("Start Date", value=datetime.today())
+    end_date = st.date_input("End Date", value=datetime.today())
     chat_type = st.selectbox("Chat Type", ["WhatsApp", "Signal"])
     newsletter = st.checkbox("Generate Newsletter Introduction")
 
     # Generate Summary
     if st.button("Generate Summary"):
-        if not chat_export_file or not summary_file or not start_date or not end_date:
+        if not chat_export_file or not summary_file:
             st.error("Please fill in all the required fields.")
+        elif start_date > end_date:
+            st.error("Start Date should be earlier than End Date.")
         else:
+            # Save the uploaded file locally
+            with open(chat_export_file.name, "wb") as file:
+                file.write(chat_export_file.getbuffer())
+
             st.info("Generating Summary...")
-            main(chat_type, chat_export_file, summary_file, start_date, end_date, newsletter)
+            start_date_str = start_date.strftime("%m/%d/%Y")
+            end_date_str = end_date.strftime("%m/%d/%Y")
+            main(chat_type, chat_export_file.name, summary_file, start_date_str, end_date_str, newsletter)
             st.success("Summary generated successfully!")
 
 # Run the app
